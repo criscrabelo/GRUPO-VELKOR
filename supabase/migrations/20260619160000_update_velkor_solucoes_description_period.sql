908ca311cd0c1a3c4cd75e713b53b561915bc963
criscrabelo <268464410+criscrabelo@users.noreply.github.com>
2026-06-19 12:45:56 -0300

    Fix: Add period to VELKOR business unit description

---
+++ supabase/migrations/20260619160000_update_velkor_solucoes_description_period.sql
@@ -0,0 +1,3 @@
+UPDATE public.business_units
+SET description = 'Regularização patrimonial, due diligence, leilões e transações seguras.'
+WHERE name IN ('VELKOR Soluções', 'VELKOR Soluções Imobiliárias');
